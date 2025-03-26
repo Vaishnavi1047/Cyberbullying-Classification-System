3d7d45bc0b5b38f2cedf0b60540ca8a545ffa056
Vartika-505 <147298977+Vartika-505@users.noreply.github.com>
2025-03-26 11:45:45 +0530

    add load model fuctionality

--- Models/BERT_model_pipeline.py
+++ Models/BERT_model_pipeline.py
@@ -2,12 +2,12 @@ import torch
 from transformers import BertTokenizer, BertForSequenceClassification
 import os
 from sklearn.metrics import classification_report
+
 class BERT_model:
     def _init_(self, tokenizer="bert-base-uncased", model_name='bert-base-uncased'):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.tokenizer = BertTokenizer.from_pretrained(tokenizer)
         self.model = BertForSequenceClassification.from_pretrained(model_name,num_labels=2).to(self.device)
-
 
  def save_model(self,output_dir='cyberbullying_model/'):
         import os
@@ -16,5 +16,12 @@ class BERT_model:
         torch.save(self.model.state_dict(), os.path.join(output_dir, 'model_state.pt'))
         self.model.save_pretrained(output_dir)
         self.tokenizer.save_pretrained(output_dir)
-        
         print(f"Model and tokenizer saved to {output_dir}")
+
+
+def load_model(self,model_path='cyberbullying_model/'):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")        
+        self.tokenizer = BertTokenizer.from_pretrained(model_path)
+        # self.model = BertForSequenceClassification.from_pretrained(model_path).to(self.device)        
+        # self.model.load_state_dict(torch.load(os.path.join(model_path,'model_state.pt'), map_location=self.device))
+        self.model = BertForSequenceClassification.from_pretrained(model_path, num_labels=2).to(self.device)
